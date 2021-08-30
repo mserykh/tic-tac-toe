56fc028c57f55a8692500875c154925b03eb2c67
Marie Serykh <marie.serykh@gmail.com>
2021-08-30 16:14:39 +0200

    feat: add partial solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,45 @@
 class TicTacToe {
     constructor() {
-
     }
 
-    getCurrentPlayerSymbol() {
+    currentPlayer = 'x';
+    matrix = [
+        [null, null, null],
+        [null, null, null], 
+        [null, null, null]
+    ];
+    rowIndex = null;
+    columnIndex = null;
 
+    getCurrentPlayerSymbol() {
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.matrix[rowIndex][columnIndex] === null) {
+            this.matrix[rowIndex][columnIndex] = this.currentPlayer;
+            this.currentPlayer = (this.currentPlayer === 'x' ? 'o' : 'x');
+        }
     }
 
     isFinished() {
-
+        return this.isDraw() || Boolean(this.getWinner());
     }
 
     getWinner() {
-
+        return null;
     }
 
     noMoreTurns() {
-
+        return this.matrix.some(row => row.includes(null) === true);
     }
 
     isDraw() {
-
+        return this.noMoreTurns() && !Boolean(this.getWinner());
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex];
     }
 }
 
