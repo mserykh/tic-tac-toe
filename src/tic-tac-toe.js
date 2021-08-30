bd5f388dd7239458a9cfe42a754bbc07bfa427fc
Marie Serykh <marie.serykh@gmail.com>
2021-08-30 17:13:06 +0200

    feat: add solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -23,19 +23,47 @@ class TicTacToe {
     }
 
     isFinished() {
-        return this.isDraw() || Boolean(this.getWinner());
+        return Boolean(this.getWinner()) || this.isDraw();
     }
 
     getWinner() {
-        return null;
+        let winner = null;
+        if (this.matrix[0][0] === this.matrix[1][1] && this.matrix[0][0] === this.matrix[2][2] && this.matrix[0][0] !== null) {
+            winner = this.matrix[0][0];
+        }
+        else if (this.matrix[2][0] === this.matrix[1][1] && this.matrix[2][0] === this.matrix[0][2] && this.matrix[2][0] !== null) {
+            winner = this.matrix[2][0];
+        }
+        else if (this.matrix[0][0] === this.matrix[1][0] && this.matrix[0][0] === this.matrix[2][0] && this.matrix[0][0] !== null) {
+            winner = this.matrix[0][0];
+        }
+        else if (this.matrix[0][1] === this.matrix[1][1] && this.matrix[0][1] === this.matrix[2][1] && this.matrix[0][1] !== null) {
+            winner = this.matrix[0][1];
+        }
+        else if (this.matrix[0][2] === this.matrix[1][2] && this.matrix[0][2] === this.matrix[2][2] && this.matrix[0][2] !== null) {
+            winner = this.matrix[0][2];
+        }
+        else if (this.matrix[0][0] === this.matrix[0][1] && this.matrix[0][0] === this.matrix[0][2] && this.matrix[0][0] !== null) {
+            winner = this.matrix[0][0];
+        }
+        else if (this.matrix[1][0] === this.matrix[1][1] && this.matrix[1][0] === this.matrix[1][2] && this.matrix[1][0] !== null) {
+            winner = this.matrix[1][0];
+        }
+        else if (this.matrix[2][0] === this.matrix[2][1] && this.matrix[2][0] === this.matrix[2][2] && this.matrix[2][0] !== null) {
+            winner = this.matrix[2][0];
+        }
+        else {
+            winner = null;
+        }
+        return winner;
     }
 
     noMoreTurns() {
-        return this.matrix.some(row => row.includes(null) === true);
+        return !this.matrix.some(row => row.includes(null) === true);
     }
 
     isDraw() {
-        return this.noMoreTurns() && !Boolean(this.getWinner());
+        return this.noMoreTurns() && !this.getWinner();
     }
 
     getFieldValue(rowIndex, colIndex) {
